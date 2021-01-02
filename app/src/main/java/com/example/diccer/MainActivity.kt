757830b0b143cb31d13efe3e1aab75282024aeb2
Ashish gautam <ashish30adev@gmail.com>
2021-01-02 22:23:13 +0530

    anim added

--- app/src/main/java/com/example/diccer/MainActivity.kt
+++ app/src/main/java/com/example/diccer/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.diccer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-lateinit var diceView : ImageView
+    lateinit var diceView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceView = findViewById(R.id.diceView)
-        val rollBtn : Button = findViewById(R.id.rollBtn)
+        val rollBtn: Button = findViewById(R.id.rollBtn)
 
-        rollBtn.setOnClickListener{
+        rollBtn.setOnClickListener {
+            val fadeIn =AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_in)
+            val fadeOut =AnimationUtils.loadAnimation(this@MainActivity, R.anim.fade_out)
+           diceView.startAnimation(fadeIn)
             RollADice()
+            diceView.startAnimation(fadeOut)
         }
 
     }
 
     private fun RollADice() {
         val diceInt = Random().nextInt(6) + 1
-        val diceImages = when(diceInt){
-            1-> R.drawable.ic__dice1
-            2-> R.drawable.ic__dice2
-            3-> R.drawable.ic__dice3
-            4-> R.drawable.ic__dice4
-            5-> R.drawable.ic__dice5
+        val diceImages = when (diceInt) {
+            1 -> R.drawable.ic__dice1
+            2 -> R.drawable.ic__dice2
+            3 -> R.drawable.ic__dice3
+            4 -> R.drawable.ic__dice4
+            5 -> R.drawable.ic__dice5
             else -> R.drawable.ic__dice6
         }
+
         diceView.setImageResource(diceImages)
     }
 }
